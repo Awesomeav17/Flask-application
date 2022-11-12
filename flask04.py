6004149813619c5d475337563a217fd4bd01e8bd
Awesomeav17 <93501313+Awesomeav17@users.noreply.github.com>
2022-11-11 23:18:19 -0500

    flask app #4 - updated queries to return one

--- flask04.py
+++ flask04.py
@@ -38,14 +38,14 @@ def index():
     return render_template('index.html', user = a_user)
 @app.route('/notes')
 def get_notes(): 
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='aozarkar@uncc.edu') 
     my_notes = db.session.query(Note).all()
 
     return render_template('notes.html', notes=my_notes, user = a_user)
 @app.route('/notes/<note_id>')
 def get_note(note_id): 
     a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
-    my_note = db.session.query(Note).filter_by(id=note_id)
+    my_note = db.session.query(Note).filter_by(id=note_id).one() 
 
     return render_template('note.html', note=my_note , user = a_user)
 
